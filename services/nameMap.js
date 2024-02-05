6c2ba591f6445a51dab10bcda250bc79d5ee4d59
jerryshi <jerryshi042003@gmail.com>
2024-02-05 14:56:42 -0800

    Changed Name Map to simpler display

--- services/nameMap.js
+++ services/nameMap.js
@@ -3,35 +3,56 @@
    to the right side. */
    
 const nameMap = {
-    // Data Version 0 (Points)
-    "point_winner"           : 'Point Winner',
+   // New Player1 Focuse Map
+    "player1ServePlacement"  : 'Serve Placement',
+    "player1ServeResult"     : 'Serve Result',
 
-    "serverName"             : 'Server',
-    "Serve Result"           : 'Serve Result',
-    "Serve Placement"        : 'Serve Placement',
-    "servingSide"            : 'Duece/Ad Side',
+    "player1ReturnPlacement" : 'Return Placement',
+    "player1ReturnFhBh"      : 'Return Forehand/Backhand',
+    "player1LastShotResult"  : 'Winner/Error',
 
-    "returnerName"           : 'Returner',
-    "Return (Forehand/Backhand)": 'Return (Forehand/Backhand)',
-    "Return Placement"       : 'Return Placement',
-    "Return Error Type"      : 'Return Error Type',
+    "player1LastShotPlacement" : 'Last Shot Placement',
+    "player1LastShotPlacementFhBh" : 'Last Shot Forehand/Backhand',
 
-    "Serve +1 Forehand/Backhand" : 'Serve +1 Forehand/Backhand',
-    "Serve +1 Placement"     : 'Serve +1 Placement',
-    "Serve +1 Error Type"    : 'Serve +1 Error Type',
-
-   //  "Return +1 Forehand/Backhand" : 'Return +1 Forehand/Backhand',
-   //  "Return +1 Placement"    : 'Return +1 Placement',
-   //  "Return +1 Error Type"   : 'Return +1 Error Type',
-
-    "Last Shot Forehand/Backhand" : 'Last Shot Forehand/Backhand',
-    "Last Shot Placement"    : 'Last Shot Placement',
-    "Last Shot Error Type"   : 'Last Shot Error Type',
-
-    "atNetName"                  : 'At Net',
-    "isbreakPoint"           : 'Break Point',
-    "setScoreSum"            : 'Set #',
     "rallyCountFreq"         : 'Rally Length',
+    "atNetPlayer1"           : 'At Net',
+    "pointWonBy"             : 'Point Won By',
+    "side"                   : 'Duece/Ad Side',
+    "setNum"                 : 'Set Number',
+   
+
+
+
+
+    // Data Version 0 (Points)
+   //  "point_winner"           : 'Point Winner',
+
+   //  "serverName"             : 'Server',
+   //  "Serve Result"           : 'Serve Result',
+   //  "Serve Placement"        : 'Serve Placement',
+   //  "servingSide"            : 'Duece/Ad Side',
+
+   //  "returnerName"           : 'Returner',
+   //  "Return (Forehand/Backhand)": 'Return (Forehand/Backhand)',
+   //  "Return Placement"       : 'Return Placement',
+   //  "Return Error Type"      : 'Return Error Type',
+
+   //  "Serve +1 Forehand/Backhand" : 'Serve +1 Forehand/Backhand',
+   //  "Serve +1 Placement"     : 'Serve +1 Placement',
+   //  "Serve +1 Error Type"    : 'Serve +1 Error Type',
+
+   // //  "Return +1 Forehand/Backhand" : 'Return +1 Forehand/Backhand',
+   // //  "Return +1 Placement"    : 'Return +1 Placement',
+   // //  "Return +1 Error Type"   : 'Return +1 Error Type',
+
+   //  "Last Shot Forehand/Backhand" : 'Last Shot Forehand/Backhand',
+   //  "Last Shot Placement"    : 'Last Shot Placement',
+   //  "Last Shot Error Type"   : 'Last Shot Error Type',
+
+   //  "atNetName"                  : 'At Net',
+   //  "isbreakPoint"           : 'Break Point',
+   //  "setScoreSum"            : 'Set #',
+   //  "rallyCountFreq"         : 'Rally Length',
     
 
 
