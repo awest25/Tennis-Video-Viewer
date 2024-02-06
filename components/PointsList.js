546bbc045b599ea10ee478f4e96444d7959443ce
tomohto <toh2737@gmail.com>
2024-02-05 19:06:59 -0800

    Updated points list to show game score and set number

--- components/PointsList.js
+++ components/PointsList.js
@@ -4,11 +4,15 @@ import React from 'react';
 import styles from '../styles/PointsList.module.css';
 
 const PointsList = ({ pointsData, onPointSelect }) => {
+    const calculateTotalSets = (setScore) => {
+        const scores = setScore.split('-').map(Number);
+        return scores.reduce((a, b) => a + b, 0) + 1;
+    }
     return (
         <ul className={styles.pointsList}>
             {pointsData.map((point, index) => (
                 <li className={styles.pointsListItem} key={index} onClick={() => onPointSelect(point.Position)}>
-                    {point.Name}
+                    {"Score: " + point.Name + " Games: " + point.gameScore + " Set: " + calculateTotalSets(point.setScore)}
                 </li>
             ))}
         </ul>
