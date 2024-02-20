523a525f3865a6f2fbd4b2c3500ffa521547c50d
Frederick Zhang <frederickzhang@ucla.edu>
2024-02-19 16:29:56 -0800

    intial thoughts

--- services/initializeFirebase.js
+++ services/initializeFirebase.js
@@ -2,6 +2,26 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+/*
+multiple ways to confuigure dev vs prod
+1. (best imo) add test property to matches and automitically filter
+  - doesn't need to be that clear of a separation between test-matches and matches; just delete afterwards
+  - Firebase endpoint is only accessed on home page (SearchDropdown) and potentially tag-match page
+    - easy to filter: small-scale project
+  - no need to manually change the .env code every time
+    - less user errors
+2. new Firebase collection with mathes
+  - (X) firebase serve: local firebase configs
+  - (tentative X) firebase alias: firebase version env vars, just an alias for project-id
+    - must switch on CLI: firebase use dev; firebase use staging, etc
+    - deployment: uses currently selected project (conflicts with GitHub Actions auto-deploy; better for large-scale projects)
+  - (X) initializeApp(config, 'secondary'): multiple databases db1, db2, etc
+    - suitable for different db, not dev/prod because DB endpoints
+  - (would work) .env vars: apiKey: process.env.FIREBASE_API_KEY,
+    - need to change .env var and npm run dev
+    - look into Firebase .env support (set auto true/false during production)
+*/
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBOvEiiwKE_Q_aTWdyOpTIDmYmbPHA9KOg",
@@ -20,5 +40,16 @@ const db = getFirestore(app);
 
 // Get the Firebase Storage service
 const storage = getStorage(app);
+
+/*
+// secondary
+const secondaryAppConfig = {
+  // credential: cert(secondaryServiceAccount),
+  databaseURL: 'https://match-viewing-dashboard.firebaseio.com'
+};
+const secondary = initializeApp(secondaryAppConfig, 'secondary');
+const secondaryDb = getFirestore(secondary);
+const secondaryStorage = getStorage(secondary);
+*/
 
 export { db, storage }; // Export both Firestore and Storage
