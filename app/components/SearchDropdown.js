7b2bd1d98414a95dedcc36a9941924bb3cae801a
Frederick Zhang <frederickzhang@ucla.edu>
2024-02-28 00:00:58 -0800

    cleaned version

--- app/components/SearchDropdown.js
+++ app/components/SearchDropdown.js
@@ -20,11 +20,9 @@ const SearchDropdown = () => {
             const querySnapshot = await getDocs(collection(db, 'matches'));
             const matches = querySnapshot.docs.map((doc) => {
                 const data = doc.data();
-                console.log(data)
                 return {
                     value: data.videoId, // Use 'value' to adhere to react-select convention
-                    label: data.name,
-                    ...data
+                    label: data.name
                 };
             });
             setDropdownData(matches);
@@ -39,12 +37,8 @@ const SearchDropdown = () => {
         fetchMatches();
     }, [videoId]); // Depend on the matchId URL parameter
 
-    // ... (other code remains unchanged)
-
     const handleDropdownItemClick = async (option) => {
         setSelectedOption(option); // Update the selected option state
-        const matchId = option.value; // Access the value property directly
-        // await router.push(`/matches/${matchId}`);
         await router.push('/matches/' + option.value);
     };
 
@@ -55,6 +49,11 @@ const SearchDropdown = () => {
                 onChange={handleDropdownItemClick}
                 value={selectedOption} // Set the selected option based on the URL
                 options={dropdownData}
+                components={{
+                  NoOptionsMessage: () => (
+                    <div className={styles.loader}></div>
+                  ),
+                }}
                 className={styles.searchDropdown}
             />
         </div>
@@ -62,92 +61,3 @@ const SearchDropdown = () => {
 }
 
 export default SearchDropdown;
-
-
-/**
- * version 2: using URL to parse videoId
- * TODO: update this so react-select isn't highlighting everything blue
- 
-'use client'
-
-import React, { useState, useEffect } from 'react';
-import Select, { components } from 'react-select';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../services/initializeFirebase.js';
-import styles from '../styles/SearchDropdown.module.css';
-import { useRouter, usePathname } from 'next/navigation'
-
-const SearchDropdown = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [dropdownData, setDropdownData] = useState([]);
-
-  const router = useRouter();
-  const pathname = usePathname();
-  const videoId = pathname.substring(pathname.lastIndexOf('/') + 1);
-
-  // Fetch the matches from the database and format them for the dropdown
-  useEffect(() => {
-    const fetchMatches = async () => {
-      const querySnapshot = await getDocs(collection(db, 'matches'));
-      // const matches = querySnapshot.docs.map(doc => ({ 
-      //   label: doc.name,
-      //   value: doc.id,
-      //   doc.data() }));
-      const matches = querySnapshot.docs.map((doc) => {
-        const data = doc.data();
-        return {
-            value: doc.videoId, // Use 'value' to adhere to react-select convention
-            label: data.name,
-            ...data
-        };
-      });
-      // use formatOptions
-      setDropdownData(matches);
-    };
-
-    fetchMatches();
-
-    // const matchName = (dropdownData.find(item => item.videoId === videoId) || {label: ''}).label;
-    // setSearchTerm(matchName);
-  }, []);
-
-  // useEffect(() => {
-  //   const matchName = (dropdownData.find(item => item.videoId === videoId) || {label: ''}).label;
-  //   setSearchTerm(matchName);
-  // }, [videoId])
-
-  const handleDropdownItemClick = (selectedOption) => {
-    router.push('/matches/' + selectedOption.videoId);
-    console.log(selectedOption)
-    setSearchTerm(selectedOption.value);
-  };
-
-  const formatOptions = (data) => {
-    return data.map((item) => ({
-      label: item.name,
-      value: item.id,
-      videoId: item.videoId
-    }));
-  };
-
-  return (
-    <div>
-      <Select
-        placeholder="Search for a tennis match..."
-        value={searchTerm}
-        components={{
-          NoOptionsMessage: () => (
-            <div className={styles.loader}></div>
-          ),
-        }}
-        onChange={handleDropdownItemClick}
-        options={dropdownData}
-        className={styles.searchDropdown}
-      />
-    </div>
-  );
-}
-
-export default SearchDropdown;
-
- */
