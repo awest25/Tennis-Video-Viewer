d8f1d04c0208990bb981d5511c24d7fe7bb244a6
Krish Patel <krishhnpatel@gmail.com>
2024-03-08 18:58:15 -0800

    Basic changes made such as splitting the game, server, points, etc, ui/ux needs some imprvoemts, don't merge

--- app/components/PointsList.js
+++ app/components/PointsList.js
@@ -1,25 +1,74 @@
-// components/PointsList.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/PointsList.module.css';
 
 const PointsList = ({ pointsData, onPointSelect }) => {
-  // Switched for Point Title Calculated in Notebook
-  // const calculateTotalSets = (setScore) => {
-  //     const scores = setScore.split('-').map(Number);
-  //     return scores.reduce((a, b) => a + b, 0) + 1;
-  // }
+    const [expanded, setExpanded] = useState(false); // State to track whether rows are expanded
 
-  return (
-    <ul className={styles.pointsList}>
-      {pointsData.map((point, index) => (
-        <li className={styles.pointsListItem} key={index} onClick={() => {onPointSelect(point.Position)}} >
-          {point.Name}
-        </li>
-                
-      ))}
-    </ul>
-  );
+    const parsePointData = (pointName) => {
+        const regex = /Set (\d+): (\d+-\d+), (\d+-\d+) (.*?) Serving/;
+        const match = pointName.match(regex);
+
+        if (match) {
+            const [, set, gameScore, pointScore, serverName] = match;
+            return { set, gameScore, pointScore, serverName };
+        }
+
+        return { set: '', gameScore: '', pointScore: '', serverName: '' };
+    };
+
+    const visiblePointsData = expanded ? pointsData : pointsData.slice(0, 5); // Display only 5 elements initially or all if expanded
+
+    return (
+        <table className={styles.pointsList}>
+            <thead>
+                <tr>
+                    <th>Server</th>
+                    <th>Set</th>
+                    <th>Game</th>
+                    <th>Point</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {visiblePointsData.map((point, index) => {
+                    const pointText = point.Name || '';
+                    const { set, gameScore, pointScore, serverName } = parsePointData(pointText);
+
+                    return (
+                        <tr className={styles.pointsListItem} key={index}>
+                            <td>{serverName}</td>
+                            <td>{set}</td>
+                            <td>{gameScore}</td>
+                            <td>{pointScore}</td>
+                            <td>
+                                {/* Display resized play button image */}
+                                <button
+                                  className={styles.circularButton}
+                                  onClick={(event) => {
+                                      event.stopPropagation();
+                                      onPointSelect(index);
+                                  }}>▶</button>
+                            </td>
+                        </tr>
+                    );
+                })}
+                {!expanded && (
+                    <tr>
+                        <td colSpan="5" className={styles.expandButtonCell}>
+                            <button className={styles.expandButton} onClick={() => setExpanded(true)}>Expand</button>
+                        </td>
+                    </tr>
+                )}
+                {expanded && (
+                    <tr>
+                        <td colSpan="5" className={styles.expandButtonCell}>
+                            <button className={styles.collapseButton} onClick={() => setExpanded(false)}>Collapse</button>
+                        </td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    );
 }
 
 export default PointsList;
