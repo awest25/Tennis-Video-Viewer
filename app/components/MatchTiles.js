54bebdddb4c2d2a4ccec6facb8e2461ede6a10c0
Frederick Zhang <frederickzhang@ucla.edu>
2024-03-16 12:58:21 -0700

    refactor code (clean + css)
    - removed height as state
    - modularized functions outside of main component for clarity

--- app/components/MatchTiles.js
+++ app/components/MatchTiles.js
@@ -1,6 +1,76 @@
 import React, { useState, useEffect } from "react";
 import styles from "../styles/MatchTiles.module.css";
 
+const extractSetScore = (setObject) => {
+  // no third set
+  if (!setObject) return { score: "", type: "" };
+  const setType = (setObject.tiebreakScore !== null && setObject.tiebreakScore !== "" && setObject.tiebreakScore !== undefined) ? "tiebreakScore" : "gameScore";
+  return {
+    // score: setType === "gameScore" ? setObject.gameScore : setObject.tiebreakScore,
+    score: setObject.gameScore,
+    type: setType
+  };
+};
+
+// score: '5-6', score[0] is 5 and score[2] is 6
+const extractPlayerFinalScores = (setScores, playerName, i) => {
+  return setScores.map(setScore => ({
+    score: parseInt(setScore.score[i]),
+    playerName: playerName
+  }));
+};
+
+//Calculate winner of match
+const calculateWinner = (playerOne, playerTwo) => {
+  const playerOneTotal = playerOne.reduce((total, current) => {
+    if (!isNaN(current.score)) {
+      return total + current.score;
+    } else {
+      return total;
+    }
+  }, 0);
+  const playerTwoTotal = playerTwo.reduce((total, current) => {
+    if (!isNaN(current.score)) {
+      return total + current.score;
+    } else {
+      return total;
+    }
+  }, 0);
+  return playerOneTotal > playerTwoTotal;
+};
+
+// Retrieve team information
+const isWomensTeam = (match) => {
+  return match.includes("(W)");
+};
+
+//Retrieve Match Date
+const extractDateFromString = (inputString) => {
+  const regex = /\b(\d{1,2}\/\d{1,2}\/\d{2,4})\b/g;
+  const matches = inputString.match(regex);
+  if (matches) {
+    // Assuming there might be multiple date patterns in the string, return an array of matches
+    const firstMatch = matches[0]; // Assuming you want to pick the first matched date
+    const dateParts = firstMatch.split("/");
+    const month = parseInt(dateParts[0]);
+    const day = parseInt(dateParts[1]);
+    const year = parseInt(dateParts[2]);
+
+    // Create a new Date object with the extracted components
+    const dateObject = new Date(year + 2000, month - 1, day);
+
+    const formattedDate = dateObject.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+
+    return formattedDate;
+  } else {
+    return null;
+  }
+};
+
 const MatchTiles = ({
   matchName,
   finalScore,
@@ -9,97 +79,80 @@ const MatchTiles = ({
   matchDetails,
 }) => {
   //Tile heights
-  const [height, setHeight] = useState(0);
   const [isUnfinished, setIsUnfinished] = useState(false);
-  //Keep container heights proportional to width
-  useEffect(() => {
-    const updateHeight = () => {
-      const container = document.querySelector("." + styles.matchInfoContainer);
-      const width = container.offsetWidth;
-      const newHeight = width * 0.35;
-      setHeight(newHeight);
-    };
-
-    // Call the updateHeight function when the component mounts
-    updateHeight();
-
-    // Add event listener for window resize to update height
-    window.addEventListener("resize", updateHeight);
-
-    // Remove event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", updateHeight);
-    };
-  }, []); // Empty dependency array ensures the effect runs only once
-
-  // Page rerender
-  useEffect(() => {
-    return () => {
-      setIsUnfinished(false);
-    };
-  }, [finalScore]);
 
   //Extract Final Scores from Each Set
   const firstSetObject = finalScore.filter((score) => score.setNum === 1).pop();
   const secondSetObject = finalScore.filter((score) => score.setNum === 2).pop();
   const thirdSetObject = finalScore.filter((score) => score.setNum === 3).pop();
   const setObjects = [firstSetObject, secondSetObject, thirdSetObject];
-  // Extract Scores and type of Each Set
-  const firstSetScore =
-        firstSetObject.tiebreakScore !== null &&
-            firstSetObject.tiebreakScore !== "" &&
-            firstSetObject.tiebreakScore !== undefined
-          ? { score: firstSetObject.gameScore, type: "tiebreakScore" }
-          : { score: firstSetObject.gameScore, type: "gameScore" };
+  // // Extract Scores and type of Each Set
+  const setScores = [extractSetScore(firstSetObject), extractSetScore(secondSetObject), extractSetScore(thirdSetObject)];
+  console.log(setScores)
+/**
+   // Extract Scores and type of Each Set
+   const firstSetScore =
+   firstSetObject.tiebreakScore !== null &&
+       firstSetObject.tiebreakScore !== "" &&
+       firstSetObject.tiebreakScore !== undefined
+     ? { score: firstSetObject.gameScore, type: "tiebreakScore" }
+     : { score: firstSetObject.gameScore, type: "gameScore" };
   const secondSetScore =
-        secondSetObject.tiebreakScore !== null &&
-            secondSetObject.tiebreakScore !== "" &&
-            secondSetObject.tiebreakScore !== undefined
-          ? { score: secondSetObject.gameScore, type: "tiebreakScore" }
-          : { score: secondSetObject.gameScore, type: "gameScore" };
-    //Check if a third set was played, then extract score.
+    secondSetObject.tiebreakScore !== null &&
+        secondSetObject.tiebreakScore !== "" &&
+        secondSetObject.tiebreakScore !== undefined
+      ? { score: secondSetObject.gameScore, type: "tiebreakScore" }
+      : { score: secondSetObject.gameScore, type: "gameScore" };
+  //Check if a third set was played, then extract score.
   const thirdSetScore = thirdSetObject
-    ? thirdSetObject.tiebreakScore !== null &&
-            thirdSetObject.tiebreakScore !== "" &&
-            thirdSetObject.tiebreakScore !== undefined
-      ? { score: thirdSetObject.tiebreakScore, type: "tiebreakScore" }
-      : { score: thirdSetObject.gameScore, type: "gameScore" }
-    : { score: "", type: "" };
+  ? thirdSetObject.tiebreakScore !== null &&
+        thirdSetObject.tiebreakScore !== "" &&
+        thirdSetObject.tiebreakScore !== undefined
+  ? { score: thirdSetObject.tiebreakScore, type: "tiebreakScore" }
+  : { score: thirdSetObject.gameScore, type: "gameScore" }
+  : { score: "", type: "" };
   const setScores = [firstSetScore, secondSetScore, thirdSetScore];
+  console.log(setScores)
+ */
 
   // Extract Player Names and Assign Scores for Each Player
   // player1 is client, player2 is opponent
   const playerOneName = firstSetObject.player1Name;
   const playerTwoName = firstSetObject.player2Name;
+  const playerOneFinalScores = extractPlayerFinalScores(setScores, playerOneName, 0);
+  const playerTwoFinalScores = extractPlayerFinalScores(setScores, playerTwoName, 2);
+  /**
   const playerOneFinalScores = [
     {
-      score: parseInt(firstSetScore.score[0]),
-      playerName: firstSetObject.player1Name,
+      score: parseInt(setScores[0].score[0]),
+      playerName: playerOneName,
     },
     {
-      score: parseInt(secondSetScore.score[0]),
-      playerName: firstSetObject.player1Name,
+      score: parseInt(setScores[1].score[0]),
+      playerName: playerOneName,
     },
     {
-      score: parseInt(thirdSetScore.score[0]),
-      playerName: firstSetObject.player1Name,
+      score: parseInt(setScores[2].score[0]),
+      playerName: playerOneName,
     },
   ];
   const playerTwoFinalScores = [
     {
-      score: parseInt(firstSetScore.score[2]),
-      playerName: firstSetObject.player2Name,
+      score: parseInt(setScores[0].score[2]),
+      playerName: playerTwoName,
     },
     {
-      score: parseInt(secondSetScore.score[2]),
-      playerName: firstSetObject.player2Name,
+      score: parseInt(setScores[1].score[2]),
+      playerName: playerTwoName,
     },
     {
-      score: parseInt(thirdSetScore.score[2]),
-      playerName: firstSetObject.player2Name,
+      score: parseInt(setScores[2].score[2]),
+      playerName: playerTwoName,
     },
   ];
-    //Tiebreaker scores array
+   */
+
+  //Tiebreaker scores array
   const playerOneTieScores = Array(3);
   const playerTwoTieScores = Array(3);
 
@@ -114,16 +167,13 @@ const MatchTiles = ({
     playerTwoTieScores,
     index
   ) => {
-    if (setScores[index].type !== "tiebreakScore") {
+    if (setScores[index].type == "gameScore") {
       //Check if unfinished match
-      if (playerOneFinalScores[index].score < 5 &&
-                playerTwoFinalScores[index].score < 5 && !isUnfinished) {
+      if (playerOneFinalScores[index].score < 5 && playerTwoFinalScores[index].score < 5 && !isUnfinished) {
         setIsUnfinished(true);
       }
       //Increment winners score by 1
-      if (
-        playerOneFinalScores[index].score > playerTwoFinalScores[index].score
-      ) {
+      if (playerOneFinalScores[index].score > playerTwoFinalScores[index].score) {
         playerOneFinalScores[index].score++;
       } else {
         playerTwoFinalScores[index].score++;
@@ -133,8 +183,7 @@ const MatchTiles = ({
       playerOneTieScores[index] = parseInt(setObjects[index].tiebreakScore[0]);
       playerTwoTieScores[index] = parseInt(setObjects[index].tiebreakScore[2]);
       //check if unfinished match
-      if (playerOneFinalScores[index].score < 5 &&
-                playerTwoFinalScores[index].score < 5 && !isUnfinished) {
+      if (playerOneFinalScores[index].score < 5 && playerTwoFinalScores[index].score < 5 && !isUnfinished) {
         setIsUnfinished(true);
       }
       if (playerOneTieScores[index] > playerTwoTieScores[index]) {
@@ -167,63 +216,9 @@ const MatchTiles = ({
     );
   });
 
-  //Calculate winner of match
-  const calculateWinner = (playerOne, playerTwo) => {
-    const playerOneTotal = playerOne.reduce((total, current) => {
-      if (!isNaN(current.score)) {
-        return total + current.score;
-      } else {
-        return total;
-      }
-    }, 0);
-    const playerTwoTotal = playerTwo.reduce((total, current) => {
-      if (!isNaN(current.score)) {
-        return total + current.score;
-      } else {
-        return total;
-      }
-    }, 0);
-    return playerOneTotal > playerTwoTotal;
-  };
-
-  // Retrieve team information
-  const isWomensTeam = (match) => {
-    return match.includes("(W)");
-  };
-
-  //Retrieve Match Date
-  const extractDateFromString = (inputString) => {
-    const regex = /\b(\d{1,2}\/\d{1,2}\/\d{2,4})\b/g;
-    const matches = inputString.match(regex);
-    if (matches) {
-      // Assuming there might be multiple date patterns in the string, return an array of matches
-      const firstMatch = matches[0]; // Assuming you want to pick the first matched date
-      const dateParts = firstMatch.split("/");
-      const month = parseInt(dateParts[0]);
-      const day = parseInt(dateParts[1]);
-      const year = parseInt(dateParts[2]);
-
-      // Create a new Date object with the extracted components
-      const dateObject = new Date(year + 2000, month - 1, day);
-
-      const formattedDate = dateObject.toLocaleDateString("en-US", {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-      });
-
-      return formattedDate;
-    } else {
-      return null;
-    }
-  };
-
   return (
     <div className={styles.matchTilesContainer}>
-      <div
-        className={styles.matchInfoContainer}
-        style={{ height: `${height}px` }}
-      >
+      <div className={styles.matchInfoContainer}>
         <div className={styles.containerTitle}>Final Score</div>
         {/* Player Information  */}
         <div className={styles.playerInfo}>
@@ -249,7 +244,6 @@ const MatchTiles = ({
                   ? { opacity: "40%" }
                   : { opacity: "100%" }
             }
-
           >
             {/* Check if tie break, if so add exponent */}
             {playerOneFinalScores.map((score, index) =>
@@ -328,10 +322,7 @@ const MatchTiles = ({
         </div>
       </div>
       {/* Match Location */}
-      <div
-        className={styles.matchInfoContainer}
-        style={{ height: `${height}px` }}
-      >
+      <div className={styles.matchInfoContainer}>
         <div className={styles.containerTitle}>Match Information</div>
         <div className={styles.containerInfo}>{matchDetails}</div>
         <div className={styles.containerInfo}>
@@ -339,13 +330,10 @@ const MatchTiles = ({
         </div>
       </div>
       {/* School Info */}
-      <div
-        className={styles.matchInfoContainer}
-        style={{ height: `${height}px` }}
-      >
+      <div className={styles.matchInfoContainer}>
         <div className={styles.containerTitle}>Matchup</div>
         <div className={styles.containerInfo}>
-                    UCLA {isWomensTeam(matchName) && "(Womens)"}
+          UCLA {isWomensTeam(matchName) && "(Womens)"}
         </div>
         <div className={styles.containerInfo}>
           {finalScore[0].opponentTeam} {isWomensTeam(matchName) && "(Womens)"}
--- app/services/getLogos.js
+++ app/services/getLogos.js
@@ -4,18 +4,18 @@ import { ref, listAll, getDownloadURL } from "firebase/storage"; // Import stora
 const logosRef = ref(storage, 'logos');
 
 const getLogos = async() => {
-    try {
-        const files = await listAll(logosRef);
-        const logos = await Promise.all(files.items.map(async (item) => {
-            const downloadURL = await getDownloadURL(item);
-            const parsedName = item.name.split('.')[0];
-            return { name: parsedName, downloadURL };
-        }));
-        return logos;
-      } catch (error) {
-        console.error('Error retrieving files:', error);
-        throw error;
-      }
+  try {
+    const files = await listAll(logosRef);
+    const logos = await Promise.all(files.items.map(async (item) => {
+      const downloadURL = await getDownloadURL(item);
+      const parsedName = item.name.split('.')[0];
+      return { name: parsedName, downloadURL };
+    }));
+    return logos;
+  } catch (error) {
+    console.error('Error retrieving files:', error);
+    throw error;
+  }
 };
 
 export default getLogos;
