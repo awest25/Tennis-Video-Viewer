529ff4716aa3a7e4dac01564b5301625fc461951
ritzz26 <ritagarwalrit@gmail.com>
2024-04-18 00:28:04 -0700

    fixed the DOM structure

--- app/components/ExtendedList.js
+++ app/components/ExtendedList.js
@@ -1,87 +1,43 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from '../styles/ExtendedList.module.css';
+import playButtonImage from '../play_circle.jpg';
 
-//create a table with dynamic width and height
-function createDynamicTable(data, client, opp) {
-    const table = document.createElement("table");
-    table.style.width = "100%";
-    table.classList.add("dynamic-table");
-
-    const thead = document.createElement("thead");
-    const headerRow = document.createElement("tr");
-
-    //TABLE HEADER, TBD, USING KEYS FOR NOW
+const ExtendedList = ({ pointsData, client_logo, opponent_logo }) => {
     let keys = ["", "serverName", "setScore", "gameScore", "pointScore", "pointWonBy", "lastShotResult", "rallyCount"];
     let keys_headers = ["", "Server", "Set Score", "Game Score", "Point", "Point Winner", "Last Shot Type", "Duration"];
-    keys_headers.forEach((key) => {
-        const th = document.createElement("th");
-        th.textContent = key;
-        th.style.fontSize = '20px';
-        th.style.fontWeight = 'normal';
-        th.style.textAlign= 'left';
-        headerRow.appendChild(th);
-    });
-
-    thead.appendChild(headerRow);
-    table.appendChild(thead);
-
-    const tbody = document.createElement("tbody");
-    data.forEach((item) => {
-        const row = document.createElement("tr");
-        keys.forEach((key, index) => {
-            const td = document.createElement("td");            
-            const img = document.createElement("img");
-            //NOTE: the player1 is always client historically. THIS CODE IS LEGACY CHANGE IF NEEDED
-            if(index==0){
-            console.log(item['player1Name'])
-                if(item['player1Name']===item['serverName']){
-                    img.src = client;
-                }
-                else{
-                    img.src = opp;
-                }
-                img.width="35";
-                td.appendChild(img);
-            }
-            else{
-                td.textContent = item[key];
-            td.style.fontSize='20px';
-            td.style.fontWeight = 'bold';
-            td.style.textAlign= 'left';
-            td.style.padding = "8px";
-            td.style.borderBottom="solid 0.2px #EDEDED";
-            td.style.padding = "20px";
-            }
-            row.appendChild(td);
-        });
-        // if (index !== data.length - 1) {
-            // row.style.border='5px';
-        // }
-        tbody.appendChild(row);
-    });
-
-    table.appendChild(tbody);
-
-    return table;
-}
-
-const ExtendedList = ({ pointsData, client, opp }) => {
-    useEffect(() => {
-        const tableContainer = document.getElementById("table-container");
-        const dynamicTable = createDynamicTable(pointsData, client, opp);
-        const tableWrapper = document.createElement("div");
-        tableWrapper.classList.add(styles.tableContainer); 
-        tableWrapper.appendChild(dynamicTable);
-        tableContainer.appendChild(tableWrapper);
-        // return () => {
-        //     if (tableContainer && tableWrapper) {
-        //         tableContainer.removeChild(tableWrapper);
-        //     }
-        // };
-    }, [pointsData]);
+    
 
     return (
         <div id="table-container" className={styles.tableContainer}>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        {keys_headers.map((key, index) => (
+                            <th className={styles.TH} key={index}>{key}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {pointsData.map((item, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {keys.map((key, cellIndex) => (
+                                <td className={styles.TD} key={cellIndex}>
+                                    {cellIndex === 0 ? (
+                                        <img src={item['player1Name'] === item['serverName'] ? client_logo : opponent_logo} className={styles.IMG}/>
+                                    ) : (
+                                        item[key]
+                                    )}
+                                </td>
+                            ))}
+                            <td>
+                                <button className={styles.button}>
+                                    <img src={playButtonImage} className={styles.playButton} />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
