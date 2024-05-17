f81de88897d569b7fbccaeeb88c2c40aa73c5e92
Alexander Chen <alexander.chen750@gmail.com>
2024-05-17 10:47:38 -0700

    Dashboard Component: Added functions, may need more testing when more matches in db

--- app/(interactive)/dashboard/page.js
+++ app/(interactive)/dashboard/page.js
@@ -7,18 +7,44 @@ import transformData from '../../services/transformData';
 //import styles from '../../styles/Dashboard.module.css';
 import MatchTiles from '@/app/components/MatchTiles.js';
 import extractSetScores from '@/app/services/extractSetScores.js';
-import {  doc, documentId, getDoc } from 'firebase/firestore';
+import {  doc, documentId, getDoc , getDocs, collection} from 'firebase/firestore';
+
+const groupByDate = (matches) => {
+    const grouped = {};
+    // Group matches into arrays by date
+    matches.forEach(match => {
+        const date = match.matchDate || 'unknown';
+        if (!grouped[date]) {
+            grouped[date] = [];
+        }
+        grouped[date].push(match);
+    });
+
+    // Convert the object into an array of arrays and sort by date descending
+    return Object.entries(grouped).sort((a, b) => {
+        // Convert date strings to Date objects for comparison
+        // 'unknown' dates are treated as the earliest possible date
+        const dateA = a[0] === 'unknown' ? new Date(0) : new Date(a[0]);
+        const dateB = b[0] === 'unknown' ? new Date(0) : new Date(b[0]);
+        return dateB - dateA; // Sort descending
+    }).map(entry => entry[1]); // Extract only the arrays of matches
+};
+
+
+
+
 
 const tileList = () => {
     const [matchList, setMatchList] = useState([]);
     const [matchData, setMatchData] = useState();
-    const [matchTemp, setmatchTemp] = useState();
-    const matchSetScores = matchData ? extractSetScores(matchData.points) : {};
+    const [matchTemp, setMatchTemp] = useState();
     
+    const matchSetScores = matchData ? extractSetScores(matchData.points) : {};
+    /*
     const docId = "9y4JTHWmT62kfmNJUhfQ";
 
     useEffect(() => {
-        console.log("matchData:", matchData)
+       // console.log("matchData:", matchData)
         const fetchData = async () => {
 
         try {
@@ -46,8 +72,9 @@ const tileList = () => {
     }, [matchData]);
 
     useEffect(() => {
-        console.log("Updated matchList:", matchList);  // This will log the updated state
-    }, [matchList]);  // This effect runs whenever matchList changes
+        console.log("Updated mathTemp:", matchList);  // This will log the updated state
+        console.log("matchData:", matchData)
+    }, [matchTemp]);  // This effect runs whenever matchList changes
 
     useEffect(() => {
         const fetchData = async () => {
@@ -64,7 +91,33 @@ const tileList = () => {
 
         fetchData(); // Call the fetchData function when the component mounts or when videoId changes
     }, []);
+    */
 
+
+    const splitMatches = () => {
+
+    }
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+          try {
+            const querySnapshot = await getDocs(collection(db, 'UCLA (W)'));
+            // Iterate over each document, transform the data, and collect the results
+            const transformedMatches = querySnapshot.docs.map(doc => {
+                const data = doc.data(); // Get the data of each document
+                return transformData(data); // Assuming transformData is a function you have that processes each document's data
+            });
+            const sortedDate = groupByDate(transformedMatches)
+            console.log(sortedDate)
+            setMatchList(sortedDate); // Update your state with the transformed matches
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          } 
+        };
+    
+        fetchMatches();
+    }, []);
+      
 
     return (
         <div /*className={styles.matchContainer}*/>
@@ -73,7 +126,12 @@ const tileList = () => {
                             opponentTeam={matchData.opponentTeam}
                             matchDetails={matchData.matchDetails}
                             {...matchSetScores} />)}
-            {matchList && (<DashboardMatch matchInfo = {matchList}/>)}
+
+            {matchList && matchList.map((matches, index) => (
+                matches.length > 0 && (
+                    <DashboardMatch key={index} matchInfo={matches} />
+                )
+            ))}
         </div>
     )
 }
--- app/components/DashboardMatch.js
+++ app/components/DashboardMatch.js
@@ -1,11 +1,128 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MatchTiles from "./MatchTiles";
 import extractSetScores from '@/app/services/extractSetScores';
 import styles from '../styles/DashboardMatch.module.css'; // Import the CSS module
 
+const calculateMatchDate = (matchInfo) => {
+    if (matchInfo && matchInfo.length > 0 && matchInfo[0].matchDate) {
+        const date = matchInfo[0].matchDate;
+        const formattedDate = date.slice(5, 7) + '/' + date.slice(8, 10) + '/' + date.slice(0, 4);
+        return formattedDate;
+    }
+    return "Date unavailable";
+}
+
+const matchTitle = (matchInfo) => {
+    if (matchInfo && matchInfo.length > 0) {
+        const clientTeam = matchInfo[0].teams.clientTeam;
+        const opponentTeam = matchInfo[0].teams.opponentTeam;
+
+        let clientWins = 0;
+        let opponentWins = 0;
+
+        matchInfo.forEach(match => {
+            let setClientWins = 0;
+            let setOpponentWins = 0;
+            match.sets.forEach(set =>{
+                if(set.clientGamesWon > set.opponentGamesWon)
+                {
+                    setClientWins++;
+                }
+                else{
+                    setOpponentWins++;
+                }
+            })
+            if(setClientWins > setOpponentWins)
+            {
+                clientWins++;
+            }
+            else{
+                opponentWins++;
+            }
+
+        });
+
+        const result = clientWins > opponentWins ? 'W' : 'L';
+        const formattedTitle = `${clientTeam} vs. ${opponentTeam} - ${result}(${clientWins}-${opponentWins})`;
+        return formattedTitle;
+    }
+    return "Game Bugged";
+};
+
+
+
+
+
+const DashboardMatch = ({ matchInfo }) => { 
+    const singlesMatches = matchInfo.filter(match => match.singles === true);
+    const doublesMatches = matchInfo.filter(match => match.singles === false);
+
+    return (    
+        <div className={styles.parentContainer}>
+            <div className={styles.header}>
+                <h1 className={styles.title}>{matchTitle(matchInfo)}</h1>
+                <h1 className={styles.date}>{calculateMatchDate(matchInfo)}</h1>
+            </div>
+            {singlesMatches && singlesMatches.length > 0 && (
+                <div>
+                    <h2 className={styles.matchTypeTitle}>Singles</h2>
+                    <div className={styles.matchesContainer}>
+                        {singlesMatches && singlesMatches.map((match, index) => {
+                        const matchSetScores = match ? extractSetScores(match.points) : {};
+                        return (
+                            <div key={index} className={styles.matchItem}>
+                            <MatchTiles 
+                                matchName={match.name}
+                                clientTeam={match.clientTeam}
+                                opponentTeam={match.opponentTeam}
+                                matchDetails={match.matchDetails}
+                                {...matchSetScores}  // Spread the extracted scores into the component as props
+                                tagged={{status: match.published}}
+                                displaySections={{ score: true, info: false, matchup: false }}
+                            />
+                            </div>
+                        );
+                        })}
+                    </div>
+                </div>
+            )}
+            {doublesMatches && doublesMatches.length > 0 && (
+            <div>
+                <h2 className={styles.matchTypeTitle}>Doubles</h2>
+                <div className={styles.matchesContainer}>
+                    {doublesMatches.map((match, index) => {
+                        const matchSetScores = match ? extractSetScores(match.points) : {};
+                        return (
+                            <div key={index} className={styles.matchItem}>
+                                <MatchTiles 
+                                    matchName={match.name}
+                                    clientTeam={match.clientTeam}
+                                    opponentTeam={match.opponentTeam}
+                                    matchDetails={match.matchDetails}
+                                    {...matchSetScores}  // Spread the extracted scores into the component as props
+                                    tagged={{status: match.published}}
+                                    displaySections={{ score: true, info: false, matchup: false }}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        )}
+        </div>
+    );
+};
+
+export default DashboardMatch;
+
+/*
 const DashboardMatch = ({ matchInfo }) => { 
     const singlesMatches = matchInfo[0];
     const doublesMatches = matchInfo[1]; // Assuming you'll use this later
+
+    useEffect(() => {
+
+    });
 
     return (
         <div className={styles.parentContainer}>
@@ -25,7 +142,6 @@ const DashboardMatch = ({ matchInfo }) => {
                         opponentTeam={match.opponentTeam}
                         matchDetails={match.matchDetails}
                         {...matchSetScores}  // Spread the extracted scores into the component as props
-                        tagged={{status: true}}
                         displaySections={{ score: true, info: false, matchup: false }}
                     />
                     </div>
@@ -56,3 +172,4 @@ const DashboardMatch = ({ matchInfo }) => {
 };
 
 export default DashboardMatch;
+*/
