779ae364864870c678e7b01a498ba276b76a24e6
kesdlvi <kesdlvi12@gmail.com>
2024-05-17 01:12:20 -0700

    Fixed highlight on click and added placeholder for main content

--- app/(interactive)/matches/page.js
+++ app/(interactive)/matches/page.js
@@ -1,37 +1,82 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-import { db } from '../../services/initializeFirebase.js';
-import styles from '../../styles/MatchesDashboard.module.css';
-import RosterList from '@/app/components/RosterList.js';
+import { db } from "../../services/initializeFirebase.js";
+import styles from "../../styles/MatchesDashboard.module.css";
+import RosterList from "@/app/components/RosterList.js";
+import { collection, getDocs } from "firebase/firestore";
 
 // Please see the Figma design:
 // https://www.figma.com/file/YWXYlBgXTF60y8AfMC2LJI/bsa?type=design&node-id=605%3A405&mode=design&t=DMZnDipr5BWWn86v-1
 
 const MatchesDashboard = () => {
+  // Selecting Player from Roster
+  const [matchData, setMatchData] = useState([]);
+  const [activePlayer, setActivePlayer] = useState(null);
+
+  const handleActivePlayerChange = (player) => {
+    setActivePlayer(player);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const MatchQuerySnapshot = await getDocs(collection(db, "matches"));
+      const matches = MatchQuerySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMatchData(matches);
+    };
+    fetchData();
+  }, []);
+
+  //Get Player's matches
+  const playerMatches = matchData
+    .filter(
+      (match) =>
+        activePlayer &&
+        match.points[0].player1Name &&
+        match.points[0].player1Name ===
+          activePlayer.firstName + " " + activePlayer.lastName
+    )
+    .map((match) => match);
+
   return (
     <div className={styles.container}>
       <h1>Matches</h1>
       <div className={styles.searchSection}>
-        <input type="text" placeholder="Search matches..." className={styles.searchBox}/>
+        <input
+          type="text"
+          placeholder="Search matches..."
+          className={styles.searchBox}
+        />
         <button className={styles.searchButton}>Search</button>
       </div>
       <div className={styles.eventsContainer}>
-          {/* Dynamic event tile content goes here */}
+        {/* Dynamic event tile content goes here */}
       </div>
       <div className={styles.mainContent}>
         <div className={styles.matchesContainer}>
           {/* Dynamic match content goes here */}
+          {activePlayer && (
+            <h1>
+              {activePlayer.firstName} {activePlayer.lastName}
+            </h1>
+          )}
+          {playerMatches &&
+            playerMatches.map((match, index) => (
+              <h2 key={index}>{match.name}</h2>
+            ))}
         </div>
         <div className={styles.rosterContainer}>
           {/* Roster list goes here */}
-          <RosterList/>
+          <RosterList onActivePlayerChange={handleActivePlayerChange} />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default MatchesDashboard;
--- app/components/RosterList.js
+++ app/components/RosterList.js
@@ -3,11 +3,11 @@ import RosterTile from "./RosterTile";
 import { db } from '../services/initializeFirebase.js'; // Ensure storage is exported from initializeFirebase.js
 import { collection, getDocs } from 'firebase/firestore';
 
-const RosterList = () => {
+const RosterList = ({ onActivePlayerChange }) => {
     // create roster list then loop through
     const [mensRoster, setMensRoster] = useState([]); // State to hold the fetched teams
     const [activePlayerIndex, setActivePlayerIndex] = useState(null); // Current Player tile that is blue
-    
+
     useEffect(() => {
         const fetchTeams = async () => {
             try {
@@ -19,7 +19,10 @@ const RosterList = () => {
                     lastName: player.lastName,
                     photoUrl: player.photo
                 }));
+                // Sort by last name
+                playersArray.sort((a, b) => a.lastName.localeCompare(b.lastName));
                 setMensRoster(playersArray);
+                
             } catch (error) {
                 console.error('Error retrieving teams:', error);
             }
@@ -30,7 +33,9 @@ const RosterList = () => {
 
     // Determines index of selected player tile
     const handleTileClick = (index) => {
-        setActivePlayerIndex(index === activePlayerIndex ? null : index);
+        const newIndex = index === activePlayerIndex ? null : index;
+        setActivePlayerIndex(newIndex);
+        onActivePlayerChange(newIndex !== null ? mensRoster[newIndex] : null);
     };
 
     return (
