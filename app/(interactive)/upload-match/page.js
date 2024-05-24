048044b88f14d7e20435e6c2156c540398e475e5
Frederick Zhang <frederickzhang@ucla.edu>
2024-05-23 17:19:20 -0700

    new support for data
    client team
    Date
    Division
    Event
    lineupPosition
    match venue
    opponent team
    player1Name
    player2Name
    player1Hand
    player2Hand
    Round
    Surface
    Weather
    player1UTR
    player2UTR
    indoor/outdoor
    tagged/untagged
    match name
    published

--- app/(interactive)/upload-match/page.js
+++ app/(interactive)/upload-match/page.js
@@ -43,12 +43,24 @@ export default function UploadVideo() {
   const [clientTeam, setClientTeam] = useState();
   const [clientPlayerFirst, setClientPlayerFirst] = useState('');
   const [clientPlayerLast, setClientPlayerLast] = useState('');
+  const [clientUTR, setClientUTR] = useState(null);
   const [opponentTeam, setOpponentTeam] = useState();
   const [opponentPlayerFirst, setOpponentPlayerFirst] = useState('');
   const [opponentPlayerLast, setOpponentPlayerLast] = useState('');
+  const [opponentUTR, setOpponentUTR] = useState(null);
   const [teams, setTeams] = useState([]);
-  const [matchDate, setMatchDate] = useState('')
+  const [matchDate, setMatchDate] = useState('');
+  const [division, setDivision] = useState('');
+  const [event, setEvent] = useState('');
+  const [lineup, setLineup] = useState('');
+  const [matchVenue, setMatchVenue] = useState('');
+  const [round, setRound] = useState('');
+  const [indoor, setIndoor] = useState(false);
+  const [surface, setSurface] = useState('hard');
   const [singles, setSingles] = useState(true);
+  const [temperature, setTemperature] = useState(null);
+  const [cloudy, setCloudy] = useState(false);
+  const [windy, setWindy] = useState(false);
 
   useEffect(() => {
     const fetchTeams = async () => {
@@ -65,7 +77,7 @@ export default function UploadVideo() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     
-    if (!matchScore || !videoId || !clientTeam || !opponentTeam || !clientPlayerFirst || !clientPlayerLast || !opponentPlayerFirst || !opponentPlayerLast || !matchDate) {
+    if (!matchScore || !videoId || !clientTeam || !opponentTeam || !clientPlayerFirst || !clientPlayerLast || !opponentPlayerFirst || !opponentPlayerLast || !matchDate || !clientUTR || !opponentUTR || !temperature || !division || !event || !lineup || !matchVenue || !round || !indoor || !surface) {
       // console.error(`Please fill in the following fields: ${missingFields.join(', ')}.`);
       console.error('missing fields')
       return;
@@ -85,15 +97,32 @@ export default function UploadVideo() {
       const players = {
         client: {
           firstName: clientPlayerFirst,
-          lastName: clientPlayerLast
+          lastName: clientPlayerLast,
+          UTR: clientUTR
         },
         opponent: {
           firstName: opponentPlayerFirst,
-          lastName: opponentPlayerLast
+          lastName: opponentPlayerLast,
+          UTR: opponentUTR
         }
       };
+      const weather = {
+        temperature,
+        cloudy,
+        windy
+      }
+      const matchDetails = {
+        weather, 
+        division,
+        event,
+        lineup,
+        matchVenue,
+        round,
+        indoor,
+        surface
+      }
       const sets = parseMatchScore(matchScore);
-      await uploadMatch(sets, videoId, pointsJson, pdfFile, teams, players, matchDate, singles);
+      await uploadMatch(sets, videoId, pointsJson, pdfFile, teams, players, matchDate, singles, matchDetails);
       alert('done!')
     } catch (error) {
       console.error("Error uploading match:", error);
@@ -151,6 +180,10 @@ export default function UploadVideo() {
               {clientPlayerOptions}
             </select>
           </label>
+          <label>Client UTR:
+            <input type="text" value={clientUTR} onChange={(e) => setClientUTR(e.target.value)} />
+          </label>
+          
           <label>
             Opponent Team: 
             <select id="search" onChange={(e) => setOpponentTeam(e.target.value)}>
@@ -167,37 +200,54 @@ export default function UploadVideo() {
               {opponentPlayerOptions}
             </select>
           </label>
+          <label>Opponent UTR:
+            <input type="text" value={opponentUTR} onChange={(e) => setOpponentUTR(e.target.value)} />
+          </label>
           <label>
             Match Score (spaces only between sets): 7-4 6-7(0-7) 7-2(13-11): 
           </label>
           <input type="text" value={matchScore} onChange={(e) => setMatchScore(e.target.value)} />
           <label htmlFor="date">Date:
-            <input
-              type="date"
-              id="date"
-              value={matchDate}
-              onChange={(e) => {setMatchDate(e.target.value)}}
-            />
+            <input type="date" value={matchDate} onChange={(e) => {setMatchDate(e.target.value)}} />
+          </label>
+          <label>Division:
+            <input type="text" value={division} onChange={(e) => setDivision(e.target.value)} />
+          </label>
+          <label>Event:
+            <input type="text" value={event} onChange={(e) => setEvent(e.target.value)} />
+          </label>
+          <label>Lineup:
+            <input type="text" value={lineup} onChange={(e) => setLineup(e.target.value)} />
+          </label>
+          <label>Match Venue:
+            <input type="text" value={matchVenue} onChange={(e) => setMatchVenue(e.target.value)} />
+          </label>
+          <label>Round:
+            <input type="text" value={round} onChange={(e) => setRound(e.target.value)} />
           </label>
           <label>
             Video ID: 
             <input type="text" value={videoId} onChange={(e) => setVideoId(e.target.value)} />
           </label>
-          <label>
-            <input
-              type="radio"
-              checked={singles}
-              onChange={() => {setSingles(true)}}
-            />
-            Singles
+          <label>Temperature:
+            <input type="text" value={temperature} onChange={(e) => setTemperature(e.target.value)} />
           </label>
-          <label>
-            <input
-              type="radio"
-              checked={!singles}
-              onChange={() => {setSingles(false)}}
-            />
-            Doubles
+          <label> Weather: 
+            <input type="checkbox" checked={cloudy} onChange={() => {setCloudy(!cloudy)}} /> Cloudy
+            <input type="checkbox" checked={windy} onChange={() => {setWindy(!windy)}} /> Windy
+          </label>
+          <label> Court: 
+            <input type="radio" checked={!indoor} onChange={() => {setIndoor(false)}} /> Outdoor
+            <input type="radio" checked={indoor} onChange={() => {setIndoor(true)}} /> Indoor
+          </label>
+          <label> Surface: 
+            <input type="radio" checked={surface === 'hard'} onChange={() => {setSurface('hard')}} /> Hard
+            <input type="radio" checked={surface === 'clay'} onChange={() => {setSurface('clay')}} /> Clay
+            <input type="radio" checked={surface === 'grass'} onChange={() => {setSurface('grass')}} /> Grass
+          </label>
+          <label> Singles/Doubles
+            <input type="radio" checked={singles} onChange={() => {setSingles(true)}} /> Singles
+            <input type="radio" checked={!singles} onChange={() => {setSingles(false)}} /> Doubles
           </label>
           <label>
             JSON File: 
