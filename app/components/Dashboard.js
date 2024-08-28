c4a9de8d6b872125479a0f3607bbadd5c7fd5378
kesdlvi <kesdlvi12@gmail.com>
2024-08-28 14:54:10 -0700

    add match search with react select

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,14 +1,18 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
-import { useMatchData } from './MatchDataProvider';
-import styles from '../styles/Dashboard.module.css';
-import DashTileContainer from './DashTileContainer';
-import getTeams from '@/app/services/getTeams.js';
-import RosterList from './RosterList.js';
+import React, { useEffect, useState, useMemo } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { useMatchData } from "./MatchDataProvider";
+import styles from "../styles/Dashboard.module.css";
+import DashTileContainer from "./DashTileContainer";
+import getTeams from "@/app/services/getTeams.js";
+import RosterList from "./RosterList.js";
+import Select from "react-select";
+import FuzzySearch from "./fuzzySearch";
+import SearchPlaceholder from "./SearchPlaceholder";
+import { SelectStyles } from "./SelectStyles";
 // Import sample data to test data fetching
-import matchData from '../(interactive)/dashboard/sampleData';
+import matchData from "../(interactive)/dashboard/sampleData";
 
 // Extract date from match name
 const extractDateFromName = (name) => {
@@ -25,14 +29,14 @@ const extractDateFromName = (name) => {
 
 // Format date based on type
 export const formatDate = (date, formatType) => {
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
   const year = date.getFullYear();
 
   switch (formatType) {
-    case 'MM/DD/YY':
+    case "MM/DD/YY":
       return `${month}/${day}/${String(year).slice(-2)}`;
-    case 'MM/DD/YYYY':
+    case "MM/DD/YYYY":
       return `${month}/${day}/${year}`;
     default:
       throw new Error(`Unknown format type: ${formatType}`);
@@ -48,20 +52,19 @@ const formatMatches = (matches) =>
       return {
         ...match,
         date: date,
-        formattedDate: date ? formatDate(date, 'MM/DD/YYYY') : null,
+        formattedDate: date ? formatDate(date, "MM/DD/YYYY") : null,
       };
     })
     .sort((a, b) => (b.date && a.date ? b.date - a.date : 1));
 
-
-
 const Dashboard = () => {
-  // const { matches, error } = useMatchData(); // Using the custom hook to access match data
+  //const { matches, error } = useMatchData(); // Using the custom hook to access match data
   const matches = matchData; // using hardcoded JSON objects
   const router = useRouter();
   const formattedMatches = formatMatches(matchData);
   const [logos, setLogos] = useState({}); // Store logos for each opponent team
-  
+  const [searchTerm, setSearchTerm] = useState("");
+
   // Group matches by date
   const matchesByDate = formattedMatches.reduce((acc, match) => {
     const matchDate = match.formattedDate;
@@ -80,16 +83,18 @@ const Dashboard = () => {
       try {
         const allTeams = await getTeams();
         const logosMap = {};
-        formattedMatches.forEach(match => {
+        formattedMatches.forEach((match) => {
           const opponentTeam = match.opponentTeam;
-          const opponentLogoURL = allTeams.find((team) => team.name === opponentTeam)?.logoUrl;
+          const opponentLogoURL = allTeams.find(
+            (team) => team.name === opponentTeam
+          )?.logoUrl;
           if (opponentLogoURL) {
             logosMap[opponentTeam] = opponentLogoURL;
           }
         });
         setLogos(logosMap); // Set the state with the map of team logos
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
     fetchLogos();
@@ -99,19 +104,80 @@ const Dashboard = () => {
     router.push(`/matches/${videoId}`);
   };
 
+  //Styles and functions used for search, put into different file later
+
+  //Fuzzy Search Method
+  const filteredMatches = useMemo(() => {
+    if (!searchTerm) return [];
+
+    const result = formattedMatches.reduce((acc, match) => {
+      // Define the fields and their properties to search
+      const fieldsToSearch = [
+        { property: "clientTeam", value: String(match.clientTeam) },
+        { property: "clientPlayer", value: String(match.clientPlayer) },
+        { property: "opponentTeam", value: String(match.opponentTeam) },
+        { property: "opponentPlayer", value: String(match.opponentPlayer) },
+        { property: "date", value: match.date ? String(match.date) : "" },
+        { property: "singlesDoubles", value: String(match.singlesDoubles) },
+        { property: "videoID", value: String(match.videoID) },
+      ];
+
+      // Find matched fields
+      const matchedFields = fieldsToSearch.filter(({ value }) =>
+        FuzzySearch(searchTerm, value)
+      );
+
+      if (matchedFields.length > 0) {
+        acc.push({
+          match,
+          matchedProperties: matchedFields.map(({ property, value }) => ({
+            property,
+            value,
+          })),
+        });
+      }
+
+      return acc;
+    }, []);
+    return result; // returns array of matches from fuzzy search
+  }, [searchTerm, formattedMatches]);
+
+  const handleSearch = (inputValue) => {
+    setSearchTerm(inputValue);
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <h1>BSA | Tennis Consulting</h1>
-        <h2>Dashboard</h2>
+        <div className={styles.headerContent}>
+          <h2>Dashboard</h2>
+          <Select
+            placeholder={<SearchPlaceholder />}
+            components={{
+              DropdownIndicator: () => null, // This removes the dropdown arrow
+              IndicatorSeparator: () => null, // This removes the separator next to the arrow
+              Menu: () => null, // This removes the default menu when a search is preformed
+            }}
+            styles={SelectStyles}
+            options={filteredMatches} // Use filtered matches as options
+            onInputChange={handleSearch}
+          />
+        </div>
       </header>
-      
+
       <div className={styles.carousel}>
         {Object.keys(matchesByDate).map((date, index) => (
           <div key={index} className={styles.card}>
             <div className={styles.cardContent}>
-              <img src={logos[matchesByDate[date][0].opponentTeam]} alt="Team Logo" className={styles.logo} />
-              <span className={styles.matchDate}>{formatDate(new Date(date), 'MM/DD/YY')}</span>
+              <img
+                src={logos[matchesByDate[date][0].opponentTeam]}
+                alt="Team Logo"
+                className={styles.logo}
+              />
+              <span className={styles.matchDate}>
+                {formatDate(new Date(date), "MM/DD/YY")}
+              </span>
             </div>
           </div>
         ))}
@@ -120,8 +186,12 @@ const Dashboard = () => {
       <div className={styles.mainContent}>
         <div className={styles.matchesSection}>
           {Object.keys(matchesByDate).map((date, index) => {
-            const singlesMatches = matchesByDate[date].filter(match => match.singlesDoubles === 'Singles');
-            const doublesMatches = matchesByDate[date].filter(match => match.singlesDoubles === 'Doubles');
+            const singlesMatches = matchesByDate[date].filter(
+              (match) => match.singlesDoubles === "Singles"
+            );
+            const doublesMatches = matchesByDate[date].filter(
+              (match) => match.singlesDoubles === "Doubles"
+            );
 
             return (
               <div key={index} className={styles.matchSection}>
@@ -130,8 +200,16 @@ const Dashboard = () => {
                     <h3>{`${matchesByDate[date][0].clientTeam} vs ${matchesByDate[date][0].opponentTeam}`}</h3>
                     <span className={styles.date}>{date}</span>
                   </div>
-                  <DashTileContainer matches={singlesMatches} matchType="Singles" onTileClick={handleTileClick} />
-                  <DashTileContainer matches={doublesMatches} matchType="Doubles" onTileClick={handleTileClick} />
+                  <DashTileContainer
+                    matches={singlesMatches}
+                    matchType="Singles"
+                    onTileClick={handleTileClick}
+                  />
+                  <DashTileContainer
+                    matches={doublesMatches}
+                    matchType="Doubles"
+                    onTileClick={handleTileClick}
+                  />
                 </div>
               </div>
             );
@@ -141,10 +219,9 @@ const Dashboard = () => {
         <div className={styles.rosterContainer}>
           <RosterList />
         </div>
-        
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
