08cbb4cefe30f38c2a775c5a1d1bc91986f14034
Frederick Zhang <frederickzhang@ucla.edu>
2024-10-06 14:29:54 -0700

    remove unused import

--- app/(interactive)/tag-match/[slug]/page.js
+++ app/(interactive)/tag-match/[slug]/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import VideoPlayer from '../../../components/VideoPlayer';
 import { getTaggerButtonData, columnNames } from '../../../services/taggerButtonData.js';
 import styles from '../../../styles/TagMatch.module.css';
